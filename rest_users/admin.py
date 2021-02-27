6c9a98cc0972a412353170e12fea19d5c95fd68b
sergeiKostenko <sdh1985@mail.ru>
2021-02-27 12:21:27 +0300

    added list_display_links in admin panel

--- rest_users/admin.py
+++ rest_users/admin.py
@@ -5,6 +5,7 @@ from rest_users.models import User
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'username', 'email', 'age')
+    list_display_links = ('first_name', 'last_name', 'username')
 
 
 admin.site.register(User, UserAdmin)
