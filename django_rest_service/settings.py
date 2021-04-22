4d3dedee4d22ae20f6f79ce71501c50e7f9ffaf3
sergeiKostenko <sdh1985@mail.ru>
2021-04-22 12:42:00 +0300

    created docker container

--- django_rest_service/settings.py
+++ django_rest_service/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'django_rest_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rest_service',
+        'USER': 'rest',
+        'PASSWORD': 'rest123456',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
